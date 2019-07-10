c99c9429247c583eccbe98f3c7be9f9729ddd1a8
doctamayot <doctamayot@hotmail.com>
2019-07-10 00:07:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -12,18 +12,18 @@ class App extends Component {
       <div className="demo-big-content">
       
     <Layout className="hugo">
-        <Header className="header-color" title={<a style={{textDecoration: 'none', color: 'white'}} href="/">HUGO TAMAYO ROJAS</a>} scroll>
+        <Header className="header-color" title={<a style={{textDecoration: 'none', color: 'white'}} href={process.env.PUBLIC_URL + "/"}>HUGO TAMAYO ROJAS</a>} scroll>
             <Navigation>
-                <a href="/resume">About Me</a>
-                <a href="/projects">Projects</a>
-                <a href="/contact">Contact</a>
+                <a href={process.env.PUBLIC_URL + "/resume"}>About Me</a>
+                <a href={process.env.PUBLIC_URL +"/projects"}>Projects</a>
+                <a href={process.env.PUBLIC_URL +"/contact"}>Contact</a>
             </Navigation>
         </Header>
         <Drawer  title={<Link style={{textDecoration: 'none', color: 'black'}} to="/">HUGO TAMAYO ROJAS</Link>}>
             <Navigation>
-              <a href="/resume">Resume</a>
-              <a href="/projects">Projects</a>
-              <a href="/contact">Contact</a>
+              <a href={process.env.PUBLIC_URL + "/resume"}>Resume</a>
+              <a href={process.env.PUBLIC_URL + "/projects"}>Projects</a>
+              <a href={process.env.PUBLIC_URL + "/contact"}>Contact</a>
             </Navigation>
         </Drawer>
         <Content>
