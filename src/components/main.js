6b15fd55ffd5609027c5e098f5bbc4fd1dab172e
doctamayot <doctamayot@hotmail.com>
2019-07-09 23:02:25 -0500

    first commit

--- src/components/main.js
+++ src/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from './landingpage';
 import Contact from './contact';
 import Projects from './projects';
@@ -8,7 +8,9 @@ import Resume from './resume';
 
 const Main = () => (
   <Switch>
-    <Route exact path="/" component={LandingPage} />    
+    <BrowserRouter basename="portfolio"  >
+    <Route exact path="/" component={LandingPage} /> 
+    </BrowserRouter>   
     <Route path="/contact" component={Contact} />
     <Route path="/projects" component={Projects} />
     <Route path="/resume" component={Resume} />
